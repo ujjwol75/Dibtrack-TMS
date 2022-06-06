a1f177a7d24a4a09224c6bec06db6f6a5714181e
SunumaYangdenLimbu <sunumayengden08@gmail.com>
2022-06-06 15:20:13 +0545

    ..

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Form from './components/Form';
 import {BrowserRouter , Routes , Route} from 'react-router-dom'
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Topnav from './components/Topnav/Topnav';
 
 function App() {
   return (
@@ -15,6 +14,7 @@ function App() {
          <Routes>
            <Route path = "/login" element = {<Login />} />
            <Route path  = "/dashboard" element = {<Dashboard />}/>
+           <Route path = "/topnav" element={<Topnav/>}/>
           </Routes>
          
          </BrowserRouter> 
