d7dff150e49325793a182e73450ed2f5fb1ce76b
prakash <prakashthapa617@gmail.com>
2022-06-10 07:42:09 +0545

    ..

--- src/components/List/Ltodo.tsx
+++ src/components/List/Ltodo.tsx
@@ -23,7 +23,7 @@ const Ltodo = (props: Props) => {
                     <p className='text-sm text-gray-400 font-light mx-2 hover:text-blue-400'>{item}</p>
                 </div>
 
-                <div className='flex  w-[16rem] space-x-12 ml-8 '>
+                <div className='flex  w-[16rem] space-x-12  '>
                     {/* <IdentificationIcon className='h-6 w-6 text-sm text-gray-400 font-light  cursor-pointer hover:text-blue-400' > */}
                     <Popover>
 
