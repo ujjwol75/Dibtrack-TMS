c1d90174c3748e0546f1609eb2e48cb6bf896408
shasank shakya <shasankshakya2@gmail.com>
2022-06-10 10:34:39 +0545

    login incomplete

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,32 +1,27 @@
-import React, { useState } from "react";
-import {useNavigate} from 'react-router-dom'
-function Form() {
 
-   const[password, setPassword] = useState<string>('')
-   const[email , setEmail] = useState<string>('')
-    
+type Props = {
+  credentials: any
+  setCredentials: any
+  handleFunction: any
+}
 
-   const navigate =useNavigate()
-   const handleSubmit = (e:any) => {
-     e.preventDefault()
-     navigate('/dashborad')
-     console.log('email: ' , email)
-     console.log('password: ' , password)
-   }
+function Form(props: Props) {
+
+  const { handleFunction, credentials, setCredentials } = props
   return (
     <div className=" form-container  w-[400px]  bg-white-100 min-h-[400px] flex flex-col px-10 py-5 rounded-md">
       <h1 className="text-center text-2xl font-bold font-16">welcome back !</h1>
       <div className="form mt-5">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={(e) => handleFunction(e)}>
           <div className="flex flex-col justify-center">
             <label>Email:</label>
             <input
               type="text"
               name="email"
-              value={email}
+              value={credentials.email}
               placeholder="&#xf0e0;    Your email here"
               className="p-2 mt-5 text-black-100 border-solid-black[1px] border border-black-100 focus:outline-none"
-              onChange={(e)=> setEmail(e.target.value)}
+              onChange={(e) => setCredentials((prevState: any) => ({ ...prevState, email: e.target.value }))}
             />
           </div>
 
@@ -35,13 +30,13 @@ function Form() {
             <input
               type="password"
               name="password"
-              value={password}
+              value={credentials.password}
               placeholder="&#xf023; Your password here"
               className="input_password p-2 mt-5 text-black-100  border border-black-100 focus:outline-none
               focus:border-black-300"
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={(e) => setCredentials((prevState: any) => ({ ...prevState, password: e.target.value }))}
 
-            
+
             />
             <div className="input_password"></div>
           </div>
@@ -53,9 +48,9 @@ function Form() {
           <p className="text-center mt-5 text-xs">or login with SSO</p>
         </form>
         <p className="text-center mt-4 text-xs">Don't have an account ? sign up</p>
-       
+
       </div>
-      
+
     </div>
   );
 }
--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import Form from '../../components/form/Form'
 import Navbar from '../../components/Navbar'
+import APIS from '../../constants/EndPoint';
+import usePostHookNoAuth from '../../customHooks/usePostHookNoAuth';
+
 function Login() {
+
+  const navigate = useNavigate()
+  const [isPostLoading, mutate, isPostSuccess, postData, isPostError, successMsg, errorMsg] =
+    usePostHookNoAuth();
+
+  const [credentials, setCredentials] = useState<any>({
+    email: "",
+    password: ""
+  })
+
+  const handleLogin = async (e: any) => {
+    e.preventDefault()
+    console.log(credentials)
+    const url = APIS.LOGIN;
+    // try {
+    //   await mutate({ url, formData });
+    // } catch (e) {
+    //   navigate("/login");
+    //   console.log(e);
+    // }
+  };
+
+
   return (
     <div>
-      
+
       <div className="login h-[100vh] w-screen overflow-hidden ">
-      <Navbar text = "signup" text1='Dont have an account? '/>
-      <Form />
-      <div className='gradient w-[110%]  transform -rotate-12 '></div>
+        <Navbar text="signup" text1='Dont have an account? ' />
+        <Form credentials={credentials} setCredentials={setCredentials} handleFunction={handleLogin} />
+        <div className='gradient w-[110%]  transform -rotate-12 '></div>
       </div>
     </div>
   )
