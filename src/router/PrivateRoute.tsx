fb52f783ed5323439a271f109dfd5e10dacd583e
BishnuDahal <bishnu.dahal630@gmail.com>
2022-06-12 13:35:33 +0545

    ...

--- src/router/PrivateRoute.tsx
+++ src/router/PrivateRoute.tsx
@@ -2,11 +2,14 @@ import { Navigate, useNavigate } from 'react-router-dom';
 import { accessToken, getKey } from "../helpers/sessionKey";
 
 const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+
+
+  const token = accessToken()
+  console.log(token)
   const navigate = useNavigate()
-  // const token = accessToken()
-  // if (!token) {
-  //   navigate("/login")
-  // }
+  if (!token) {
+    navigate("/login")
+  }
   return children;
 };
 
