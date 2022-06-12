cf5657d946ecf8d850d26778dca8782b91d0dde1
prakash <prakashthapa617@gmail.com>
2022-06-12 14:49:22 +0545

    Home

---
+++ src/components/HomeDib/HomeDib.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const HomeDib = (props: Props) => {
+  return (
+    <>
+
+    
+    </>
+  )
+}
+
+export default HomeDib
--- src/layputs/WorkSpaceLayout.tsx
+++ src/layputs/WorkSpaceLayout.tsx
@@ -20,9 +20,30 @@ const DashboardLayout = () => {
     <>
       <div className="grid grid-cols-5">
         <Sidebar />
-        <div className="col-span-4">
-          <Topnav />
+        {/* <div className="col-span-4">
+          <Topnav /> 
           <Outlet />
+        </div> */}
+        <div className="h-[3.5rem] w-[92rem] bg-slate-600 flex justify-between">
+          <div className="flex">
+            <p>Home</p>
+          </div>
+          <div className="flex">
+            <div>
+
+          <p>Calender</p>
+            </div>
+            <div>
+
+          <p>Calender</p>
+            </div>
+            <div>
+
+          <p>Calender</p>
+            </div>
+         
+          </div>
+
         </div>
       </div>
     </>
