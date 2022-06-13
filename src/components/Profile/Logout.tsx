f06ea2c821c6bf425a88dd8f701eb9f20e0590d1
BishnuDahal <bishnu.dahal630@gmail.com>
2022-06-13 08:37:26 +0545

    Feat:Logout

--- src/components/Profile/Logout.tsx
+++ src/components/Profile/Logout.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
-sessionStorage.setItem("lastname", "dahal");
+
+
+import { useNavigate } from "react-router-dom";
+import { removeKey } from "../../helpers/sessionKey";
+
+
 type Props = {}
 
 const Logout = (props: Props) => {
-    const handleLogOut = () => {
-          sessionStorage.clear()
-    }
+  
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    sessionStorage.clear()
+    removeKey('userAuth');
+    navigate("/login");
+  }
   return (
     <div>
-        <div className="mt-3 flex flex-row mt-3 cursor-pointer" onClick={handleLogOut}>
-                        Logout
-        </div>
+      <div className="mt-3 flex flex-row  cursor-pointer" onClick={handleLogOut}>
+        Logout
+      </div>
     </div>
   )
 }
