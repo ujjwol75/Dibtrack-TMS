db84d8fa624d0ada4b96b17a5a341e600665e222
prakash <prakashthapa617@gmail.com>
2022-06-13 13:48:28 +0545

    home added

---
+++ src/components/HomeDib/Work/Todo.tsx/Today.tsx
@@ -0,0 +1,60 @@
+import { Menu } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+import React, { useState } from 'react'
+
+type Props = {}
+
+const Today = (props: Props) => {
+    const[task,setTask]=useState(false)
+    return (
+        <>
+            <div>
+                <Menu>
+                    <div className='flex'>
+
+                        <div >
+                            <Menu.Button className={"flex"}>
+                                <ChevronDownIcon
+                                    className="ml-2h-5 w-5 text-violet-200 hover:text-violet-100 inline-block"
+                                    aria-hidden="true" />
+                                <span className='ml-2'>Today</span>
+                            </Menu.Button>
+                        </div>
+                        <div className='flex justify-between w-full '>
+
+
+                            <div >
+                                <span className='ml-4 text-sm text-gray-400' >+Task</span> <span className='ml-2 text-sm text-gray-400'> +Remainder</span>
+                            </div>
+                            <div >
+
+                                <span className=' text-sm text-gray-400'>fsd</span>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <Menu.Items>
+                        <Menu.Item>
+                            {({ active }) => (
+                                <div className='w-full border-2 border-gray-200 p-6 text-center my-2'>
+                                    <div className='flex justify-center'>
+
+                                        <img src="/logo192.png" alt="" />
+                                    </div>
+                                    <p className='text-2xl'>Woohoo, Inbox zero!</p>
+                                    <p >Tasks and Reminders that are scheduled for Today will </p>
+                                    <p >appear here.</p>
+
+                                </div>
+                            )}
+                        </Menu.Item>
+
+
+                    </Menu.Items>
+                </Menu>
+            </div>
+        </>
+    )
+}
+
+export default Today
