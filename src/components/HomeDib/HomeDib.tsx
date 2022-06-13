e6178b7a7eca117b9ba54dd41004c8262577ab0e
prakash <prakashthapa617@gmail.com>
2022-06-13 07:40:09 +0545

    29

--- src/components/HomeDib/HomeDib.tsx
+++ src/components/HomeDib/HomeDib.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CalendarIcon, FlagIcon, AtSymbolIcon, InformationCircleIcon } from '@heroicons/react/solid'
 
 type Props = {}
 
@@ -6,7 +7,38 @@ const HomeDib = (props: Props) => {
   return (
     <>
 
-    
+      <div className="h-[3.5rem] w-[92rem] bg-slate-600 flex justify-between">
+        <div className="flex ">
+
+
+          <p className="p-4 my-auto">Home</p>
+
+        </div>
+        <div className="flex">
+          <div>
+            <p className="p-4 my-auto"><FlagIcon className='h-6 w-6 inline-block mb-2' /> Calender</p>
+
+          </div>
+          <div>
+            <p className="p-4 my-auto"><CalendarIcon className='h-6 w-6 inline-block mb-2' /> Calender</p>
+
+          </div>
+          <div>
+            <p className="p-4 my-auto"><AtSymbolIcon className='h-6 w-6 inline-block mb-2' /> Calender</p>
+
+          </div>
+
+
+        </div>
+
+      </div>
+      <div className='w-[92rem] bg-red-600 flex'>
+        <div className='w-3/5 p-4 '>
+          <p className='text-2xl '>LineUp<InformationCircleIcon className='w-4 h-4 inline-block text-gray-400 ' /><span className='text-gray-400 text-lg ml-1'>(0)</span></p>
+        </div>
+        <div className='w-2/5 p-4'>ffsdfa</div>
+
+      </div>
     </>
   )
 }
--- src/layputs/WorkSpaceLayout.tsx
+++ src/layputs/WorkSpaceLayout.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import Sidebar from "../components/dashboard/Sidebar";
+import HomeDib from "../components/HomeDib/HomeDib";
 import Topnav from "../components/Topnav/Topnav";
 import { accessToken } from "../helpers/sessionKey";
 
@@ -20,31 +21,11 @@ const DashboardLayout = () => {
     <>
       <div className="grid grid-cols-5">
         <Sidebar />
-        <div className="col-span-4">
+     <div className="col-span-4">
           <Topnav /> 
-          <Outlet />
-        </div>
-        {/* <div className="h-[3.5rem] w-[92rem] bg-slate-600 flex justify-between">
-          <div className="flex">
-            <p>Home</p>
-          </div>
-          <div className="flex">
-            <div>
-
-          <p>Calender</p>
-            </div>
-            <div>
-
-          <p>Calender</p>
-            </div>
-            <div>
-
-          <p>Calender</p>
-            </div>
-         
-          </div>
-
-        </div> */}
+         <Outlet />
+        {/* <HomeDib/> */}
+        </div> 
       </div>
     </>
   );
